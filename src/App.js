2e7f8a67fb280cabfd9edc46a71561d0e03050cc
Romareo Bromfield <romareobromfield@gmail.com>
2020-05-10 21:47:31 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+// import './App.css';
+const weatherApi ={
+  key: "3de7dbb3ba541b62d3f9b8b59bbd4b7c",
+  baseUrl:"https://api.openweathermap.org/data/2.5/weather"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+}
+
+function App() {  
+
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = event =>{ //getting weather details from api
+    if (event.key === "Enter"){
+      fetch(`${weatherApi.baseUrl}?q=${query}&units=metric&appid=${weatherApi.key}`)     
+      .then(response => response.json())
+      .then(result => {        
+        setWeather(result);
+        setQuery('');
+        console.log(result); //debugging
+      });
+    }
+  }
+
+  const dateBuilder= (d) =>{
+    let months = ["January", "February", "March", "April", 
+                  "May", "June", "July", "August", "September", 
+                  "October", "November", "December"
+                ];
+    let days = ["Sunday", "Monday", "Tuesday","Wednesday", 
+                "Thursday", "Friday", "Saturday"
+              ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+  const changeIcon=()=>{
+    const iconCode= weather.weather[0].icon;
+    const source=`http://openweathermap.org/img/wn/${iconCode}@2x.png`;  
+  
+    return (<img src={source} alt="" className="icon"/>);
+  }
+
+  return (    
+    <div className={(typeof weather.main != "undefined") ? (weather.weather[0].main==="Rain" ? "app-rain" : ((weather.main.temp > 16) ? 'app-warm' : 'app')) : 'app-warm'}>
+     <main>
+       <div className="search-box">
+         <input 
+         type="text" 
+         className="search-bar"
+         placeholder="Search..."
+         onChange={event => setQuery(event.target.value)}
+         value={query}
+         onKeyPress={search}
+         onLoad={search}
+         />
+       </div>
+       <div>
+       {(typeof weather.main != "undefined")? (
+        <div>         
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">{Math.round(weather.main.temp)}°C</div>          
+            <div className="weather">{weather.weather[0].main}</div>   
+            <div className="weather-icon">{changeIcon()}</div>        
+          </div>                 
+        </div>
+       ):""}
+       </div>
+     </main>
     </div>
   );
 }
